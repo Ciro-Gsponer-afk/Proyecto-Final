defc8d4f7a80c4063a8c1e46f4c663b098370f90
Ciro Gsponer <cirogsp235@gmail.com>
2025-11-11 10:21:32 -0300

    Primmer commit

--- ProyectoFinal/src/App.jsx
+++ ProyectoFinal/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const [name, Setname] = useState("");
+  const [Pais, SetPais] = useState(null); 
+  const navigate = useNavigate();
 
   return (
     <>
-     
+      <h1>INFO PAISES</h1>
+
+      <input type="text"onChange={(e) => Setname(e.target.value)} placeholder="Escribe un país..."/>
+      <button onClick={() => navigate(`/${name}`)}>Mostrar País</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
