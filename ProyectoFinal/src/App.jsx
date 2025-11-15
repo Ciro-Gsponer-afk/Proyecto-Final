07f620e5cde13093e3376e9e182afebda8ceb7cf
Ciro Gsponer <cirogsp235@gmail.com>
2025-11-15 00:11:16 -0300

    Ruteo Dinamico

--- ProyectoFinal/src/App.jsx
+++ ProyectoFinal/src/App.jsx
@@ -6,29 +6,34 @@ function App() {
   const [Nombre, SetNombre] = useState("")
   const [Lugar, SetLugar] = useState("")
   const [Opciones, SetOpciones] = useState([])
-
+  const navigate = useNavigate()
 
   useEffect(() => {
     fetch(`https://www.meteosource.com/api/v1/free/find_places?text=${Lugar}&language=en&key=m4jjqsoiezco6c7lecmmhg8qus818j18w77qmkve`)
       .then((response) => response.json())
       .then((data) => SetOpciones(data))
-      .catch((error) => console.error("Error al cargar la localidad:", error));
+      .catch((error) => console.error("Error al cargar las localidades:", error));
   }, [Lugar]);
   console.log(Opciones);
+
+  const CambiarURL = (Id) => {
+    navigate(`/${Id}`)
+  } 
+
 
   return (
     <>
       <div className="Bienvenida">
         <h1>Bienvenidos</h1></div>
 
-      <div className="Cuestionario">
-        <input onChange={(e) => { SetNombre(e.target.value) }} />
+      <div className="Cuestionario" >
+        <input onChange={(e) => { SetNombre( e.target.value) }} />
         <button onClick={ () =>{SetLugar(Nombre)  } } > Investigar </button>
       </div>
 
       <div className="lugares">
         {Opciones.map((Lugar) =>
-          <div className="Opcion">
+          <div className="Opcion" onClick={ () => CambiarURL(Lugar.place_id)} >
             <p> Pais : {Lugar.country}  </p>
             <p> Localidad : {Lugar.adm_area1} </p>
       </div>
--- ProyectoFinal/src/main.jsx
+++ ProyectoFinal/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Data from './Data.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/:Place_id" element={<Data/>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
